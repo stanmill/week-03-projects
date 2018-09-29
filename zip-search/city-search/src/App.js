b713b5a55cc85c78982c523d3e4df4a77ef0faba
stanmill <stanley.pena29@gmail.com>
2018-09-28 21:03:57 -0400

    updates

--- zip-search/city-search/src/App.js
+++ zip-search/city-search/src/App.js
@@ -2,62 +2,58 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function City(props){
+  return (<div></div>);
+}
 
-const API= "http://ctp-zip-api.herokuapp.com/";
+//creates the template that allows the user to enter an input
+function ZipSearchField(props){
+  return (<div>
+    <form>
+      <label>Zip Code: </label>
+      <input type="text" className="form-control" onChange={props.changeHandler}/>
+    </form>
+  </div>);
 
+}
 
 class App extends Component {
+
   constructor(props){
     super(props);
-  this.state={value: '',showTable: false, }
-
-    this.info = this.info.bind(this);
+    this.state = {
+      city: ""
+    }
   }
+  // pass a value to the API
+  ZipCodeChanged(event){
+    //if input less than 5 doesnt process value
+    if(event.target.value.length !==5){
+      return;
+    }
+    let url ="http://ctp-zip-api.herokuapp.com/zip/" + event.target.value;
 
-
-  info(e){
-    this.setState({value: e.target.value },()=> {
-      //fetch API
-      //if(event.target.value =>5){
-      fetch(API).
-      then((data)=> {
-          console.log(data)
-          this.tables();
-      })
-    
+    fetch(url).
+    then((response) =>{
+      console.log(response.status);
       
-    });
-
-  }
-
-  tables(){
-    return(<div>
-      <table>
-        <tr>
-          <th>this is the name of state</th>
-        </tr>
-        <tr>
-          <td>State:</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Location:</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Population:</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Total Wages:</td>
-          <td></td>
-        </tr>
-      </table>
-    </div>);
+      if(response.status == 200){
+        return response.json();
+      }else{
+        throw "Not Found";
+      }
+    })
+    .then((jsonData) => {
+      console.log(jsonData);
+      this.setState({})
+    })
+    .catch((error) => {
+      //create a console log
+     //clear previous data this.setState
+    })
   }
 
   render() {
-
 
     return (
       <div className="App">
@@ -65,13 +61,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">City Search</h1>
         </header>
-      <form>
-        <label>
-          Zip Code:
-          <input type="text" value={this.state.value} onChange={this.info}></input>
-        </label>
-      </form>
-      {this.state.showTable ? this.tables(): null};
+        <ZipSearchField changeHandler={(e) => this.ZipCodeChanged(e)}/>
+        <div>
+          <City />
+          <City />
+        </div>
       </div>
     );
   }
